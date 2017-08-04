533937ad20793da633f0f143595bf0365df09330
davischan83 <davischan83@gmail.com>
2017-08-04 04:30:28 +0300

    UI update of Home Scene

--- CVDEvaluator/ViewControllers/Evaluation/HomeController.swift
+++ CVDEvaluator/ViewControllers/Evaluation/HomeController.swift
@@ -9,31 +9,31 @@
 import UIKit
 
 
-class HomeCell: UITableViewCell {
-	@IBOutlet weak var titleLabel: UILabel!
-	@IBOutlet weak var descriptionLabel: UILabel!
-	@IBOutlet weak var icon: UIImageView!
-	@IBOutlet weak var buttonContentView: UIView!
-}
-
-
-class HomeController: BaseController, UITableViewDelegate, UITableViewDataSource {
+class HomeController: BaseController {
+	
+	@IBOutlet weak var newEvaluationView: UIView!
+	@IBOutlet weak var savedEvaluationsView: UIView!
 	
 	static let settingsSegueID = "settingsSegueID"
 	static let newEvaluationSegueID = "newEvaluationSegueID"
 	static let savedEvaluationSegueID = "savedEvaluationSegueID"
 	
-	@IBOutlet weak var tableView: UITableView!
-	
 	override var createdID: String! { return "home" }
-	
-	var cellHeight: CGFloat = 150.0
 	
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
-		tableView.tableFooterView = UIView()
+		newEvaluationView.layer.cornerRadius = 4.0
+		newEvaluationView.layer.borderWidth = 1.0
+		newEvaluationView.layer.borderColor = UIColor.lightGray.cgColor
+		newEvaluationView.clipsToBounds = true
+		
+		savedEvaluationsView.layer.cornerRadius = 4.0
+		savedEvaluationsView.layer.borderWidth = 1.0
+		savedEvaluationsView.layer.borderColor = UIColor.lightGray.cgColor
+		savedEvaluationsView.clipsToBounds = true
+
 	}
 	
 	
@@ -44,9 +44,12 @@ class HomeController: BaseController, UITableViewDelegate, UITableViewDataSource
 		
 		DataManager.manager.fetchEvaluations()
 		
-		cellHeight = min(255, (self.view.frame.size.height - 64.0) / 2.0)
-		
-		self.tableView.reloadData()
+		if DataManager.manager.hasSavedEvaluations() {
+			savedEvaluationsView.isHidden = false
+		}
+		else {
+			savedEvaluationsView.isHidden = true
+		}
 	}
 	
 	
@@ -55,9 +58,6 @@ class HomeController: BaseController, UITableViewDelegate, UITableViewDataSource
 		
 		self.navigationItem.hidesBackButton = true
 		
-		if let index = self.tableView.indexPathForSelectedRow{
-			self.tableView.deselectRow(at: index, animated: true)
-		}
 	}
 	
 	
@@ -66,74 +66,14 @@ class HomeController: BaseController, UITableViewDelegate, UITableViewDataSource
 	}
 	
 	
-	
-	// MARK: - UITableView DataSource
-	
-	func numberOfSections(in tableView: UITableView) -> Int {
-		return 1
+	@IBAction func newEvaluationAction(_ sender: Any) {
+		DataManager.manager.evaluation = Evaluation()
+		performSegue(withIdentifier: HomeController.newEvaluationSegueID, sender: nil)
 	}
 	
 	
-	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return DataManager.manager.hasSavedEvaluations() ? 2 : 1
+	@IBAction func savedEvaluationsAction(_ sender: Any) {
+		performSegue(withIdentifier: HomeController.savedEvaluationSegueID, sender: nil)
 	}
 	
-	
-	
-	// MARK: - UITableView Delegates
-	
-	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-		
-		switch indexPath.row {
-			case 0:
-				let cell = tableView.dequeueReusableCell(withIdentifier: "HomeCell", for: indexPath) as! HomeCell
-				cell.titleLabel.text = "New Evaluation".localized
-				cell.descriptionLabel.text = "Create a new evaluation\nfor a patient".localized
-				cell.icon.image = UIImage(named: "clipboard")
-				
-				return cell
-			
-			case 1:
-				let cell = tableView.dequeueReusableCell(withIdentifier: "HomeCell", for: indexPath) as! HomeCell
-				cell.titleLabel.text = "Saved Evaluations".localized
-				cell.descriptionLabel.text = "View and edit previously saved\nevaluations".localized
-				cell.icon.image = UIImage(named: "folder")
-			
-				return cell
-			
-			default:
-				return UITableViewCell()
-		}
-	}
-	
-	
-	func setButtonShadows(cell:HomeCell) {
-		let shadowSize : CGFloat = 5.0
-		let shadowPath = UIBezierPath(rect: CGRect(x: -shadowSize / 2,
-		                                           y: -shadowSize / 2,
-		                                           width: cell.buttonContentView.frame.size.width + shadowSize,
-		                                           height: cell.buttonContentView.frame.size.height + shadowSize))
-		cell.buttonContentView.layer.masksToBounds = false
-		cell.buttonContentView.layer.shadowColor = UIColor.black.cgColor
-		cell.buttonContentView.layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
-		cell.buttonContentView.layer.shadowOpacity = 0.5
-		cell.buttonContentView.layer.shadowPath = shadowPath.cgPath
-	}
-	
-	
-	func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-		return cellHeight
-	}
-
-	
-	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-		if indexPath.row == 0 {
-			DataManager.manager.evaluation = Evaluation()
-			performSegue(withIdentifier: HomeController.newEvaluationSegueID, sender: nil)
-		}
-		else {
-			performSegue(withIdentifier: HomeController.savedEvaluationSegueID, sender: nil)
-		}
-	}
-		
 }
